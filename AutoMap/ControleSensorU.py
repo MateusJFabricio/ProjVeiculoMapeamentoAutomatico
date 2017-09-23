2e7021046ba551c35f4c5e61d3b9dd637941e69f
Mateus Fabricio <fabriciomateus05@gmail.com>
2017-09-23 17:16:53 -0300

    Create ControleSensorU.py
    Retorna a distância medida pelo sensor ultrassonico

---
+++ AutoMap/ControleSensorU.py
@@ -0,0 +1,35 @@
+#Teste de leitura do sensor ultrassonico
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 17
+ECHO = 27
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.output(TRIG, 0)
+
+
+GPIO.setup(ECHO, GPIO.IN)
+
+time.sleep(0.1)
+
+print "Iniciando medida..."
+while 1:
+        GPIO.output(TRIG, 1)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, 0)
+
+        while GPIO.input(ECHO) == 0:
+                pass
+        start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+                pass
+        stop = time.time()
+
+        print (stop - start) * 17000
+        time.sleep(0.2)
+
+GPIO.cleanup
