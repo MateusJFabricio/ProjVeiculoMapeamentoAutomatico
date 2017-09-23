3033467f415906f812ee8ef8d6ab7cfce8c0819b
Mateus Fabricio <fabriciomateus05@gmail.com>
2017-09-23 17:11:45 -0300

    Criado ControleServo.py
    Estou utilizando a porta 22, com o modo BCM ativado
    Este código eu peguei de exemplo somente. Não fui eu que criei. Somente adaptei

---
+++ AutoMap/ControleServo.py
@@ -0,0 +1,30 @@
+from Tkinter import *
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.OUT)
+pwm = GPIO.PWM(22, 100)
+pwm.start(5)
+
+class App:
+	
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        scale = Scale(frame, from_=0, to=180, 
+              orient=HORIZONTAL, command=self.update)
+        scale.grid(row=0)
+
+
+    def update(self, angle):
+        duty = float(angle) / 10.0 + 2.5
+        pwm.ChangeDutyCycle(duty)
+try:
+    root = Tk()
+    root.wm_title('Servo Control')
+    app = App(root)
+    root.geometry("200x50+0+0")
+    root.mainloop()
+finally:
+    GPIO.cleanup()
